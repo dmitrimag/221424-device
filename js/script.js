ca56fd5a2a694fdba043a832e245aea06040998a
dmitrimag <dmitrimag@gmail.com>
2016-09-09 23:52:47 +0300

    Проект на сдачу 2.2

--- js/script.js
+++ js/script.js
@@ -4,27 +4,55 @@
 /* ----- VARIABLES ----- */
 /* ---------------------------------------------------------------------------------------------------- */
 
-var search = document.querySelector( ".search__text" );
-var searchButton = document.querySelector( ".search__button" );
+var contactsButton = document.querySelector( ".contacts__button" );
+var feedbackForm = document.querySelector( ".feedback" );
+var feedbackClose = document.querySelector( ".feedback__close" );
+
+/* ---------- */
+
+var contactsMap = document.querySelector( ".contacts__map" );
+var mapModal = document.querySelector( ".map" );
+var mapClose = document.querySelector( ".map__close" );
 
 /* ---------------------------------------------------------------------------------------------------- */
 /* ----- FUNCTIONS ----- */
 /* ----- All functions start with a verb with a capital letter and use CamelCase  ----- */
 /* ---------------------------------------------------------------------------------------------------- */
 
-function ToggleSearchButton()
+function ShowFeedbackForm( e )
 {
-	if ( searchButton.classList.contains("hidden") && search.value )
+	e.preventDefault();
+	feedbackForm.classList.add( "visible" );
+	feedbackForm.classList.add( "show-popup" );
+}
+
+function HideFeedbackForm()
+{
+	feedbackForm.classList.add( "hide-popup" );
+	setTimeout( function()
 	{
-		searchButton.classList.remove("hidden");
-	}
-	else
+		feedbackForm.classList.remove( "visible" );
+		feedbackForm.classList.remove( "hide-popup" );
+	}, 1000);
+}
+
+/* ---------- */
+
+function ShowMapModal( e )
+{
+	e.preventDefault();
+	mapModal.classList.add( "visible" );
+	mapModal.classList.add( "show-popup" );
+}
+
+function HideMapModal()
+{
+	mapModal.classList.add( "hide-popup" );
+	setTimeout( function()
 	{
-		if ( !search.value )
-		{
-			searchButton.classList.add("hidden");
-		}
-	}
+		mapModal.classList.remove( "visible" );
+		mapModal.classList.remove( "hide-popup" );
+	}, 1000);
 }
 
 /* ---------------------------------------------------------------------------------------------------- */
@@ -34,6 +62,10 @@ function ToggleSearchButton()
 
 (function()
 {
-	search.addEventListener( "input", ToggleSearchButton, false );
+	contactsButton.addEventListener( "click", ShowFeedbackForm, false );
+	feedbackClose.addEventListener( "click", HideFeedbackForm, false );
+
+	contactsMap.addEventListener( "click", ShowMapModal, false );
+	mapClose.addEventListener( "click", HideMapModal, false );
 }
 )();
